52844ab2e4939829002890cfdbf87b9f21ebf931
Daniel Duarte <danielduartemora27@gmail.com>
2026-01-28 12:59:15 +0100

    config(frontend): update production API base URL for Render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === 'development'
       ? 'http://localhost:5001/api'
-      : '/api',
+      : 'https://chat-app-2u3w.onrender.com',
   withCredentials: true,
 });
